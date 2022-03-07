a87d94aa5680127635238b8846a84ba7d70f13dc
Arthur Korol <artur.korol.2000.10.25@mail.ru>
2022-03-07 12:06:44 +0300

    home work #1

--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React from 'react';
+import s from './Message.module.css';
 
-function Message() {
-    return (
-        <div>
+type MessagePropsType = {
+  avatar: string;
+  name: string;
+  message: string;
+  time: string;
+};
 
-        </div>
-    )
+function Message(props: MessagePropsType) {
+  return (
+    <div className={s.message}>
+      <img src={props.avatar} className={s.avatar} />
+      <div className={s.angle} />
+      <div className={s.content}>
+        <div className={s.name}>Arthur</div>
+        <div className={s.text}>npm start нажимал?</div>
+        <div className={s.time}>20:00</div>
+      </div>
+    </div>
+  );
 }
 
-export default Message
+export default Message;
