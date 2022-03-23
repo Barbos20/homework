bf23265f56f286a443d4c74d5459328b61b8609b
Arthur Korol <artur.korol.2000.10.25@mail.ru>
2022-03-23 10:38:39 +0300

    HW12

--- src/p2-homeworks/h12/HW12.tsx
+++ src/p2-homeworks/h12/HW12.tsx
@@ -1,26 +1,34 @@
-import React from "react";
-import s from "./HW12.module.css";
+import React from 'react';
+import s from './HW12.module.css';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppStoreType } from '../h10/bll/store';
+import { changeThemeC, colorThemeType } from './bll/themeReducer';
+import SuperRadio from '../h7/common/c6-SuperRadio/SuperRadio';
 
-const themes = ['dark', 'red', 'some'];
+const themes = ['night', 'default'];
 
 function HW12() {
-    const theme = 'some'; // useSelector
-
-    // useDispatch, onChangeCallback
-
-    return (
-        <div className={s[theme]}>
-            <hr/>
-            <span className={s[theme + '-text']}>
-                homeworks 12
-            </span>
-
-            {/*should work (должно работать)*/}
-            {/*SuperSelect or SuperRadio*/}
-
-            <hr/>
-        </div>
-    );
+  const theme = useSelector<AppStoreType, colorThemeType>(
+    (state) => state.theme.themeColor
+  );
+  const dispatch = useDispatch();
+  const onChangeOption = (e: colorThemeType) => {
+    dispatch(changeThemeC(e));
+  };
+  console.log(theme);
+  return (
+    <div className={s[theme]}>
+      <hr />
+      <span className={s[theme + '-text']}>homeworks 12</span>
+      <SuperRadio
+        name={'radio1'}
+        options={themes}
+        value={theme}
+        onChangeOption={onChangeOption}
+      />
+      <hr />
+    </div>
+  );
 }
 
 export default HW12;
