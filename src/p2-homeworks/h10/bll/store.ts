2c7d3be3bfccd08d876a914be4f2bf02f36ec57c
Arthur Korol <artur.korol.2000.10.25@mail.ru>
2022-03-23 12:39:22 +0300

    HW13

--- src/p2-homeworks/h10/bll/store.ts
+++ src/p2-homeworks/h10/bll/store.ts
@@ -1,18 +1,19 @@
 import { themeReducer } from './../../h12/bll/themeReducer';
-import { combineReducers, createStore } from 'redux'
-import {loadingReducer} from './loadingReducer'
+import { combineReducers, createStore } from 'redux';
+import { loadingReducer } from './loadingReducer';
+import { requestReducer } from '../../h13/request-reducer';
 
 const reducers = combineReducers({
-    loading: loadingReducer,
-    theme: themeReducer
+  loading: loadingReducer,
+  theme: themeReducer,
+  request: requestReducer,
+});
 
-})
+const store = createStore(reducers);
 
-const store = createStore(reducers)
+export default store;
 
-export default store
-
-export type AppStoreType = ReturnType<typeof reducers>
+export type AppStoreType = ReturnType<typeof reducers>;
 
 // @ts-ignore
-window.store = store // for dev
+window.store = store; // for dev
---
+++ src/p2-homeworks/h13/HW13.tsx
@@ -0,0 +1,15 @@
+import Request from "./Request";
+import s from './HW13.module.css'
+import React from "react";
+
+
+const HW13 = () => {
+    return(
+        <div className={s.hw13}>
+            <h2>HW13</h2>
+           <Request/>
+        </div>
+    )
+}
+
+export default HW13
